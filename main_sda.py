d2bfc09d2cf4eeb8b63356eb087108bec901ca09
Ethan <ethan.lusterman@gmail.com>
2015-11-09 09:51:14 -0500

    some experiment stuff

--- main_sda.py
+++ main_sda.py
@@ -17,8 +17,8 @@ from scipy.io import wavfile
 from theano.tensor.shared_randomstreams import RandomStreams
 
 
-std = 0.001
-N = 100
+std = 0.005
+N = 500
 here = os.path.dirname(__file__)
 # fname = os.path.join(here, 'data', 'santa_clip.wav')
 # fs, x = wavfile.read(fname)
@@ -29,9 +29,9 @@ t = np.arange(fs)
 
 x = sum_signals(
     gen_signal(t, 440, fs),
-    gen_signal(t, 510, fs),
+    # gen_signal(t, 510, fs),
     gen_signal(t, 1300, fs),
-    gen_signal(t, 82, fs),
+    # gen_signal(t, 82, fs),
 )
 x = normalize(x)
 x, n = get_first_frame(x, fs, msec=50.)
@@ -51,12 +51,12 @@ datasets = [training, validating, testing]
 params = {
     'pretrain_lr': 0.001,
     'finetune_lr': 0.1,
-    'pretraining_epochs': 15,
-    'training_epochs': 50,
+    'pretraining_epochs': 20,
+    'training_epochs': 100,
     'batch_size': 5,
     'n_visible': n,
     'n_hidden': [1000, 1000, 1000],
-    'corruption_levels': [0.1, 0.2, 0.3],
+    'corruption_levels': [0.1, 0.1, 0.1],
 }
 pprint(params)
 da = train_stacked_da(datasets, **params)
@@ -74,15 +74,14 @@ print avg_testing_mse_y
 # print "base mse: %.2E" % baseline_mse
 # print "sys  mse: %.2E" % autoencode_mse
 # print "base/sys: %.2f" % (baseline_mse/autoencode_mse)
-# print "BETTER" if autoencode_mse < baseline_mse else "WORSE"
 
-# # plots
-# fig1 = P.figure()
-# ax1 = fig1.add_subplot(211)
-# ax1.plot(test_z)
-# ax1.plot(x)
-# ax2 = fig1.add_subplot(212)
-# ax2.plot(test_s)
-# ax2.plot(x)
+# plots
+fig1 = P.figure()
+ax1 = fig1.add_subplot(211)
+ax1.plot(scale(testing.get_value()[0], -1., 1.))
+ax1.plot(x)
+ax2 = fig1.add_subplot(212)
+ax2.plot(scale(da.passthrough(testing.get_value()[0]).eval(), -1., 1.))
+ax2.plot(x)
 
-# P.show()
+P.show()
